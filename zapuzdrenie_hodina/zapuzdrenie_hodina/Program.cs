55ef2e0e0db6abd59a4aed009602eb51ae53563a
Samuel <75687509+SamuelKocifaj@users.noreply.github.com>
2022-11-10 20:08:17 +0100

    Add files via upload

---
+++ zapuzdrenie_hodina/zapuzdrenie_hodina/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+using zapuzdrenie_hodina_lib;
+
+Console.WriteLine("Hello, World!");
+
+
+Frog f1 = new("Pepe", 0.38, "Martin", new Tuple<double, double, double>(0.3, 1, 1), 4, false, true);
+Frog f2 = new("Aquino", 0.35, "Marek", new Tuple<double, double, double>(7, 5, 2), 4, false, false);
+Frog f3 = new("Palo", 0.5, "Anna", new Tuple<double, double, double>(2, 1, 1), 4, true, true);
+Fish fi4 = new("Nemo", 0.08, "Martin", new Tuple<double, double, double>(5, 4, 3), true, false);
+Fish fi5 = new("Alonso", 150, "Marcela", new Tuple<double, double, double>(15, 14, 6), true, true);
+Fish fi6 = new("Seb", 0.28, "Peter", new Tuple<double, double, double>(2, 4, 3), false, false);
+
+List<Animal> animals = new() { f1, f2, f3, fi4, fi5, fi6 };
+
+
+Console.WriteLine(f1.ToString());
+
+foreach (var animal in animals)
+{
+    Console.WriteLine(animal.ToString());
+}
+
+List<Frog> frogs = new() { f1, f2, f3};
+
+foreach (var frog in frogs)
+{
+    Console.WriteLine(frog.ToString());
+}
+
---
+++ zapuzdrenie_hodina/zapuzdrenie_hodina_lib/Zralok.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zapuzdrenie_hodina_lib
+{
+    public class Zralok:WarmBlooded
+    {
+        public bool Dangerous;
+        public int PocetZubov;
+        public Zralok(string name, double weight, string owner, Tuple<double, double, double> terrariumSize, bool dangerous, int pocetzubov) : base(name, weight, owner, terrariumSize)
+        {
+            this.Dangerous = dangerous;
+            this.PocetZubov = pocetzubov;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("+++++++++++++++++++++++");
+            sb.AppendLine($"Zralek {name}");
+            sb.AppendLine($"Owner {Owner}");
+            sb.AppendLine($"PocetZubov {PocetZubov}");
+            sb.AppendLine($"Weights {Weight}kg");
+            sb.AppendLine($"Terrarium size is {calcTerrariumSize()}cm3");
+            if (Dangerous)
+                sb.AppendLine($"Je nebezpecny");
+            else
+                sb.AppendLine($"Nie je nebezpecny");
+            sb.AppendLine("+++++++++++++++++++++++");
+            return sb.ToString();
+        }
+
+
+
+
+
+
+    }
+}
