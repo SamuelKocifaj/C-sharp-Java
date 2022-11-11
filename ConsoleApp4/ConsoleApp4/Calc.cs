fd59a1ce1734b92d45769b5d6419cd673df5519f
Samuel <75687509+SamuelKocifaj@users.noreply.github.com>
2022-11-11 09:01:27 +0100

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Calc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Calc
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Calc()
+        {
+            Console.WriteLine("Zavolal sa prázdny konštruktor");
+        }
+        public Calc(int x)
+        {
+            X = x;
+            Console.WriteLine("Zavolal sa konštruktor pre X");
+        }
+        public Calc(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Console.WriteLine("Zavolal sa konštruktor pre X a Y");
+        }
+        static void DoSomething(string s)
+        {
+            Console.WriteLine(s);
+
+        }
+        static void DoSomething(string x,int a)
+        {
+            Console.WriteLine(x);
+        }
+        public void DoSomething(string x, bool b)
+        {
+            if (b==true)
+            { Console.WriteLine(x.ToUpper()); }
+            else if (b==false)
+            { Console.WriteLine(x.ToLower()); }
+        }
+
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Foo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Class1
+    {
+        public void printStr(string str)
+        {
+            Console.WriteLine(str);        
+        }
+        public void printStr(int a)
+        {
+            Console.WriteLine(a);
+        }
+
+
+    }
+}
