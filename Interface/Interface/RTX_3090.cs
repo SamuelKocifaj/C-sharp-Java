f6b17f457d5858a0501a28fe4f7fe777684ebb44
Samuel <75687509+SamuelKocifaj@users.noreply.github.com>
2022-12-09 09:17:02 +0100

    Add files via upload

---
+++ Interface/Interface/RTX_3090.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class RTX_3090 : IProduct, IManufacturer
+    {
+        public string Label { get; set ; }
+        public double Price { get; set; }
+        public string CompanyName { get; set; }
+        public string OriginCountry { get; set; }
+        public string Address { get; set; }
+    }
+    public double CalculatePriceWithTax()
+    {
+        return Price + (Price / 20);
+    }
+
+    
+
+
+}
