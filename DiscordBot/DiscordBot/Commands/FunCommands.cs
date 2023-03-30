082a17f2bdccb763ca7763c7cdf4e3094fc2a1c2
Samuel <75687509+SamuelKocifaj@users.noreply.github.com>
2023-03-30 22:40:12 +0200

    Add files via upload

---
+++ DiscordBot/DiscordBot/Commands/FunCommands.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    public class FunCommands:BaseCommandModule
+    {
+        [Command("ping")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+    }
+}
---
+++ DiscordBot/DiscordBot/ConfigJson.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("token") ]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
